6181a886ed53ead9ffdcb80d84751fe70ed5cc25
marasinipradeep <marasinipradeep@gmail.com>
2020-09-30 12:24:24 +0930

    Updates

---
+++ precache-manifest.3c44c9e250e16841d640916df626db2a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93a0519cc6ea93d1c2e2cf9189f1c9e4",
+    "url": "/deploy_React_portfolio/index.html"
+  },
+  {
+    "revision": "ea3c841807cd23fabd4b",
+    "url": "/deploy_React_portfolio/static/css/main.935cc7a1.chunk.css"
+  },
+  {
+    "revision": "9bfd4bbe9b0b840e2ab5",
+    "url": "/deploy_React_portfolio/static/js/2.419a231e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/deploy_React_portfolio/static/js/2.419a231e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea3c841807cd23fabd4b",
+    "url": "/deploy_React_portfolio/static/js/main.a1e886e0.chunk.js"
+  },
+  {
+    "revision": "40698933ada23e4cda70",
+    "url": "/deploy_React_portfolio/static/js/runtime-main.20badf38.js"
+  },
+  {
+    "revision": "5cf3ca94c22af872db649ca2be0af28d",
+    "url": "/deploy_React_portfolio/static/media/binOut.5cf3ca94.png"
+  },
+  {
+    "revision": "8d4dc596d813f7f05731c2bd83dd0811",
+    "url": "/deploy_React_portfolio/static/media/employeeDirectory.8d4dc596.png"
+  },
+  {
+    "revision": "0a60090cfbf158ecf977da642837101a",
+    "url": "/deploy_React_portfolio/static/media/musicApp.0a60090c.png"
+  },
+  {
+    "revision": "b878d8651e84c1762dd905eb5470ce5e",
+    "url": "/deploy_React_portfolio/static/media/pradeepPhoto.b878d865.jpg"
+  },
+  {
+    "revision": "4b269034d7a048f4d7d36677bea0a22b",
+    "url": "/deploy_React_portfolio/static/media/reactPortfolio.4b269034.png"
+  },
+  {
+    "revision": "900a29722fa1cb6815badff8ebab1f28",
+    "url": "/deploy_React_portfolio/static/media/weatherDashboard.900a2972.png"
+  }
+]);
